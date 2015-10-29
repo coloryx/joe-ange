4de8d976bd5437af8eef44b8a724b45af37c88a3
colorgmi <colorgmi@126.com>
2015-10-29 16:57:02 +0800

    I add profiles.clj , it's default ignored in .gitignore file

---
+++ profiles.clj
@@ -0,0 +1,4 @@
+{:profiles/dev  {:env {:database-url "mongodb://192.168.0.200/stat"}}
+ :profiles/test {:env {:database-url "mongodb://192.168.0.200/stat"}}
+ :profiles/production {:env {:database-url "mongodb://192.168.0.200/stat"}}}
+
